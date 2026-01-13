31842f9d612acbf0f75c204eb5bc04163b8adae0
Joel Gurivireddy <joelpreetam@gmail.com>
2026-01-13 15:14:38 -0600

    added input validation

--- app/schemas/user_profile.py
+++ app/schemas/user_profile.py
@@ -1,9 +1,10 @@
 # Created by Ryan Polasky | 9/20/25
 # ACM MeteorMate | All Rights Reserved
 
-from typing import Optional, Literal
+from typing import Optional, Literal, Self
 from datetime import datetime, date
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr, Field, model_validator, field_validator
+from validation_config import *
 
 Gender = Literal["female", "male", "non_binary", "prefer_not_to_say", "other"]
 Classification = Literal["freshman", "sophomore", "junior", "senior", "graduate"]
@@ -22,6 +23,25 @@ class UserProfileCreate(BaseModel):
 
     class Config:
         from_attributes = True
+    
+    @field_validator("first_name")
+    def validate_first_name(cls, first_name) -> Self:
+        if first_name > FIRST_NAME_MAX_LEN or first_name < FIRST_NAME_MIN_LEN:
+            raise ValueError(f"User's first name must be between {FIRST_NAME_MIN_LEN} and {FIRST_NAME_MAX_LEN} characters (inclusive)")
+        if any(not char.isalpha() for char in first_name):
+            raise ValueError(f"User's first name cannot contain any numbers or special characters")
+        return first_name
+
+    @field_validator("last_name")
+    def validate_last_name(cls, last_name) -> str:
+        if last_name > LAST_NAME_MAX_LEN or last_name < LAST_NAME_MIN_LEN:
+            raise ValueError(f"User's last name must be between {LAST_NAME_MIN_LEN} and {LAST_NAME_MAX_LEN} characters (inclusive)")
+        if any(not char.isalpha() for char in last_name):
+            raise ValueError(f"User's first name cannot contain any numbers or special characters")
+        return last_name
+
+    @field_validator("gender")
+    
 
 
 class UserProfileUpdate(BaseModel):
