e6ebbb49c72b33f03acee323194b161c035ea5ab
Ryan Polasky <ryanpolasky@hotmail.com>
2026-01-22 10:28:41 -0600

    relative import again

--- database.py
+++ database.py
@@ -8,7 +8,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-from config import settings
+from .config import settings
 
 logger = logging.getLogger("meteormate." + __name__)
 
