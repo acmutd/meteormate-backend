c73175fef0a3fcc73ac752b4288d8c885c7f7013
Atharva Mishra <atharvamishra956+1@gmail.com>
2026-01-23 01:01:28 -0600

    updated schemas to match array type for profile pics

--- app/schemas/user_profile.py
+++ app/schemas/user_profile.py
@@ -1,24 +1,22 @@
 # Created by Ryan Polasky | 9/20/25
 # ACM MeteorMate | All Rights Reserved
 
-from typing import Optional, Literal, Self
+from typing import Optional, Literal, Self, List
 from datetime import datetime, date
 from pydantic import BaseModel, field_validator
 from app.schemas.validation_config import (
-    FIRST_NAME_MIN_LEN,
-    FIRST_NAME_MAX_LEN,
-    LAST_NAME_MIN_LEN,
-    LAST_NAME_MAX_LEN,
-    MIN_AGE,
-    MAX_AGE
+    FIRST_NAME_MIN_LEN, FIRST_NAME_MAX_LEN, LAST_NAME_MIN_LEN, LAST_NAME_MAX_LEN, MIN_AGE, MAX_AGE
 )
 
 Gender = Literal["female", "male", "non_binary", "prefer_not_to_say", "other"]
 Classification = Literal["freshman", "sophomore", "junior", "senior", "graduate"]
 
+
 def validate_name(name, min_len, max_len, position):
     if len(name) > FIRST_NAME_MAX_LEN or len(name) < FIRST_NAME_MIN_LEN:
-            raise ValueError(f"User's {position} name must be between {min_len} and {max_len} characters (inclusive)")
+        raise ValueError(
+            f"User's {position} name must be between {min_len} and {max_len} characters (inclusive)"
+        )
     if any(not char.isalpha() for char in name):
         raise ValueError(f"User's {position} name cannot contain any numbers or special characters")
     return name
@@ -29,14 +27,14 @@ class UserProfileCreate(BaseModel):
     major: str
     classification: Classification
     bio: str
-    profile_picture_url: Optional[str] = None
+    profile_picture_url: Optional[List[str]] = None
     first_name: str
     last_name: str
     age: int
 
     class Config:
         from_attributes = True
-    
+
     @field_validator("first_name")
     def validate_first_name(cls, v) -> str:
         return validate_name(v, FIRST_NAME_MIN_LEN, FIRST_NAME_MAX_LEN, "first")
@@ -44,13 +42,11 @@ class UserProfileCreate(BaseModel):
     @field_validator("last_name")
     def validate_last_name(cls, v) -> str:
         return validate_name(v, LAST_NAME_MIN_LEN, LAST_NAME_MAX_LEN, "last")
-    
+
     @field_validator("age")
     def validate_age(cls, v):
         if v < MIN_AGE or v > MAX_AGE:
             raise ValueError(f"User's age must be between {MIN_AGE} and {MAX_AGE} years")
-    
-
 
 
 class UserProfileUpdate(BaseModel):
@@ -58,14 +54,14 @@ class UserProfileUpdate(BaseModel):
     major: Optional[str] = None
     classification: Optional[Classification] = None
     bio: Optional[str] = None
-    profile_picture_url: Optional[str] = None
+    profile_picture_url: Optional[List[str]] = None
     first_name: Optional[str] = None
     last_name: Optional[str] = None
     age: Optional[int] = None
 
     class Config:
         from_attributes = True
-    
+
     @field_validator("first_name")
     def validate_first_name(cls, v) -> str:
         return validate_name(v, FIRST_NAME_MIN_LEN, FIRST_NAME_MAX_LEN, "first")
@@ -73,11 +69,13 @@ class UserProfileUpdate(BaseModel):
     @field_validator("last_name")
     def validate_last_name(cls, v) -> str:
         return validate_name(v, LAST_NAME_MIN_LEN, LAST_NAME_MAX_LEN, "last")
-    
+
     @field_validator("age")
     def validate_age(cls, v):
         if v < MIN_AGE or v > MAX_AGE:
-            raise ValueError(f"User's age must be between {MIN_AGE} and {MAX_AGE} years (inclusive)")
+            raise ValueError(
+                f"User's age must be between {MIN_AGE} and {MAX_AGE} years (inclusive)"
+            )
 
 
 class UserProfileResponse(BaseModel):
@@ -90,7 +88,7 @@ class UserProfileResponse(BaseModel):
     first_name: str
     last_name: str
     age: int
-    profile_picture_url: Optional[str] = None
+    profile_picture_url: Optional[List[str]] = None
     bio: str
 
     class Config:
