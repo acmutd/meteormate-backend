fbd1b46f10513c7f0dfba2d718b1af1f32f17241
Atharva Mishra <atharvamishra956+1@gmail.com>
2026-01-24 01:28:30 -0600

    added reloading

--- main.py
+++ main.py
@@ -74,4 +74,4 @@ async def health_check():
 
 if __name__ == "__main__":
     logging.info("Successfully started backup!")
-    uvicorn.run(app, host="0.0.0.0", port=3000)
+    uvicorn.run("backend.main:app", host="127.0.0.1", port=3000, reload=settings.DEBUG)
