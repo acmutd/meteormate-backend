7f36d4aba41241c8590661d4ae718f35818a634c
Atharva Mishra <atharvamishra956+1@gmail.com>
2026-03-10 11:42:16 -0500

    changed table models for banlist and admins

--- models/admin.py
+++ models/admin.py
@@ -6,9 +6,8 @@ from sqlalchemy.orm import DeclarativeBase, mapped_column, Mapped
 class Banlist(DeclarativeBase):
     __tablename__ = "banlist"
 
-    user_id: Mapped[str] = mapped_column(Text, primary_key=True)
     net_id: Mapped[str] = mapped_column(Text, nullable=False)
-    banned_at: Mapped[datetime] = mapped_column(
+    created_at: Mapped[datetime] = mapped_column(
         DateTime(timezone=True), nullable=False, server_default=func.now()
     )
 
@@ -16,8 +15,7 @@ class Banlist(DeclarativeBase):
 class Admins(DeclarativeBase):
     __tablename__ = "admins"
 
-    user_id: Mapped[str] = mapped_column(Text, primary_key=True)
     net_id: Mapped[str] = mapped_column(Text, nullable=False)
-    added_at: Mapped[datetime] = mapped_column(
+    created_at: Mapped[datetime] = mapped_column(
         DateTime(timezone=True), nullable=False, server_default=func.now()
     )
