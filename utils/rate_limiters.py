fa95d355cd589a6d4162e4e369a0c623ae29289b
Atharva Mishra <atharvamishra956+1@gmail.com>
2026-04-16 18:30:09 -0500

    more variation in limits

--- utils/rate_limiters.py
+++ utils/rate_limiters.py
@@ -6,8 +6,10 @@ from pyrate_limiter import Duration, Limiter, Rate
 from fastapi_limiter.depends import RateLimiter
 
 # Survey, Profiles, and Auth Routes
-update_limiter = Limiter(Rate(1, Duration.MINUTE * 2))  # 1 request every 2 minutes for update/create endpoints
-get_limiter = Limiter(Rate(10, Duration.MINUTE))  # 10 requests per minute for get endpoints
+sensitive_updates_limit = Limiter(Rate(1, Duration.MINUTE * 2))  # 1 request every 2 minutes for sensitive update endpoints
+regular_updates_limit = Limiter(Rate(5, Duration.MINUTE))  # 5 requests per minute for regular update endpoints
+get_limit = Limiter(Rate(10, Duration.MINUTE))  # 10 requests per minute for get endpoints
 
-update_rate_limit = Depends(RateLimiter(update_limiter))
-get_rate_limit = Depends(RateLimiter(get_limiter))
+sensitive_updates_limiter = Depends(RateLimiter(sensitive_updates_limit))
+regular_updates_limiter = Depends(RateLimiter(regular_updates_limit))
+get_rate_limiter = Depends(RateLimiter(get_limit))
